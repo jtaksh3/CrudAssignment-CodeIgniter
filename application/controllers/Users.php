4610e889df446313a13c09453a6a3c3480ff0db1
jtaksh3 <jtaksh3@gmail.com>
2020-05-30 18:00:26 +0530

    Validation Changes

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -58,16 +58,36 @@ class Users extends CI_Controller {
 	}
 
 	public function update($id){
+
+		/* Load form helper */ 
+         $this->load->helper(array('form'));
+			
+         /* Load form validation library */ 
+         $this->load->library('form_validation');
+
 		$user['Name'] = $this->input->post('name');
 		$user['Email'] = $this->input->post('email');
 		$user['Mobile_no'] = $this->input->post('mobile');
 		$user['Date_of_birth'] = $this->input->post('dob');
 		$user['Pincode'] = $this->input->post('pincode');
 
-		$query = $this->users_model->updateuser($user, $id);
+		$this->form_validation->set_rules('name', 'Name', array('required'));
+		$this->form_validation->set_rules('email', 'Email', array('required', 'regex_match[/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/]', 'is_unique[users.email]'));
+		$this->form_validation->set_rules('mobile', 'Mobile no', array('required', 'regex_match[/^[6-9]\d{9}$/]', 'is_unique[users.Mobile_No]'));
+		$this->form_validation->set_rules('dob', 'Date of Birth', array('required'));
+		$this->form_validation->set_rules('pincode', 'Pincode', array('required', 'exact_length[6]'));
 
-		if($query){
-			header('location:'.base_url().$this->index());
+		if ($this->form_validation->run() == FALSE) { 
+		    echo validation_errors();
+         } 
+
+         else {
+
+     		$query = $this->users_model->updateuser($user, $id);
+
+	    	if($query){
+		    	header('location:'.base_url().$this->index());
+		    }
 		}
 	}
 
